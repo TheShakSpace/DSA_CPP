9886a1801f27d6882761ecd56bc5ef08c5daca11
Shakshi <shakshikumari2541@gmail.com>
2026-09-09 00:01:54 +0530

    operators completed

--- operators.cpp
+++ operators.cpp
@@ -12,6 +12,28 @@ cout<< (a%b) <<endl;
 
 //for getting 2.5 as output we need to typecast one of the variable to double
 cout<< (double(a)/b) <<endl;
+
+//relational operator 
+cout<< (a>b) <<endl;   //true
+cout<< (a<b) <<endl;   //false
+cout<< (a>=b) <<endl;  //true
+cout<< (a<=b) <<endl;  //false
+cout<< (a==b) <<endl;  //false
+cout<< (a!=b) <<endl;  //true
+
+//logical operator
+cout<< (a>b && a!=b) <<endl;  //true
+cout<< (a>b || a==b) <<endl;  //true
+cout<< !(a>b) <<endl;  //false
+
+//unary operator 
+int b = a++;
+cout << b <<endl;  //5
+cout << a <<endl;  //6
+
+int a= ++b;
+cout << a <<endl;  //6
+cout << b <<endl;  //6
 return 0;
 
 }
