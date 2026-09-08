54f21a63daed738d0b5144e92565b894c7e0e98c
Shakshi <shakshikumari2541@gmail.com>
2026-09-08 09:29:18 +0530

    day 1 basics completed

--- code.cpp
+++ code.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+
+    int age = 20;
+    cout << age << endl;
     return 0;
 }
+
+//boilerplate code for a simple C++ program that prints "Hello, World!" to the console.
+
+
